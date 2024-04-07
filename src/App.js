0c5c3005d2528a5bb5342dbb262d03014c17894c
PhamChiXuan1 <99774231+PhamChiXuan1@users.noreply.github.com>
2024-04-07 22:30:11 +0700

    package

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world đây là nhóm mừ xáu</h1>
+      <h1>Hello world đây là nhóm mười sáu</h1>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
